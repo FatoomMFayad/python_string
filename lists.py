3567b409cf0173db7ee24558d3cdcef06d1a31a7
ffayad <ffayad@ptcdb.edu.ps>
2024-12-30 13:27:02 +0200

    extend array

--- lists.py
+++ lists.py
@@ -1,4 +1,5 @@
 x = [2, 5, 8, 10, 55, 11, 9, 88, 100, 101]
+y = [99, 0, 33]
 
 print(x)
 print(x[1])
@@ -11,4 +12,6 @@ print(sum(x))
 x.append(102)
 print(x)
 x.insert(0,1)
+print(x)
+x.extend(y)
 print(x)
